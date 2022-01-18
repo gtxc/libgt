5bd979f194e1105491e351979e050eeca4c82793
gt <gurkanturkarslan@hotmail.com>
2022-01-18 19:30:58 +0300

    Update pass_by_reference.cpp

--- src/cpp/functions/pass_by_reference.cpp
+++ src/cpp/functions/pass_by_reference.cpp
@@ -4,6 +4,7 @@
  */
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -13,10 +14,23 @@ void swap(int &a, int &b) {
     b = tmp;
 }
 
+// pass by reference is useful even if you do not modify the element passed to function
+// by doing so, element is not copied by function, it directly reaches to the element by reference
+// to avoid storage and copy overhead
+// by implementing item passed to function as const, it is possible to avoid unwanted modify operations
+void print_vector(const vector<int> &v) {
+    for (auto item : v) {
+        cout << item << endl;
+    }
+}
+
 int main() {
     int x{10}, y{20};
     cout << x << " " << y << endl;
-    swap(&x, &y);
+    swap(x, y);
     cout << x << " " << y << endl;
+
+    vector<int> data {1,2,3,4,5};
+    print_vector(data);
     return 0;
 }
