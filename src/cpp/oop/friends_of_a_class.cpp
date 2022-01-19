1bd007a4dcbeb79ccc02835d55ca2bcc063fb58f
gt <gurkanturkarslan@hotmail.com>
2022-01-20 00:15:48 +0300

    Add friends_of_a_class.cpp structs.cpp

---
+++ src/cpp/oop/friends_of_a_class.cpp
@@ -0,0 +1,15 @@
+/*
+ * Created by gt on 1/19/22 - 11:58 PM.
+ * Copyright (c) 2022 GTXC. All rights reserved.
+ *
+ * friend
+ * a function or class that has access to private class member
+ * and, that function of or class is NOT a member of the class it is accessing
+ *
+ * function
+ */
+
+int main() {
+
+    return 0;
+}
---
+++ src/cpp/oop/structs.cpp
@@ -0,0 +1,23 @@
+/*
+ * Created by gt on 1/19/22 - 11:54 PM.
+ * Copyright (c) 2022 GTXC. All rights reserved.
+ *
+ * essentially the same as a class expect members are public by default
+ * whereas members are private by default in classes
+ *
+ * general guidelines
+ *
+ * struct
+ * use struct for passive objects with public access
+ * do not declare methods in struct
+ *
+ * class
+ * use class for active objects with private access
+ * implement getters/setters as needed
+ * implement member methods as needed
+ */
+
+int main() {
+
+    return 0;
+}
