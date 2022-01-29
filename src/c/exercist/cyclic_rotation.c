f08000685927b4e0eb9ee96e05839bcbf421ade1
gt <gurkanturkarslan@hotmail.com>
2022-01-29 05:31:35 +0300

    Update cyclic_rotation.c

--- src/c/exercist/cyclic_rotation.c
+++ src/c/exercist/cyclic_rotation.c
@@ -6,6 +6,15 @@ struct Results {
     int N;
 };
 
+void print_int_array(int arr[], size_t size) {
+    printf("[");
+    for (unsigned long i = 0; i < size; ++i) {
+        if (i == size - 1) printf("%i", arr[i]);
+        else printf("%i, ", arr[i]);
+    }
+    printf("]\n");
+}
+
 struct Results cyclicRotation(const int A[], int N, int K) {
     struct Results r;
     int *tmp = malloc(sizeof *tmp * N);
@@ -16,23 +25,17 @@ struct Results cyclicRotation(const int A[], int N, int K) {
     for (int i = 0; i < N; ++i) {
         tmp[(i + K) % N] = A[i];
     }
-
     r.A = tmp;
     r.N = N;
-
     return r;
 }
 
 int main() {
-    int array[] = {1,2,3,4,5};
-    int array_size;
-    array_size = sizeof array / sizeof array[0];
-    struct Results r;
-    r = cyclicRotation(array, array_size, 3);
-    for (int i = 0; i < array_size; ++i) {
-        printf("r.A[%i] = %i\n", i, r.A[i]);
-    }
-
-    free(r.A);
+    int A[] = {1,2,3,4,5};
+    int size = sizeof A / sizeof A[0];
+    struct Results result;
+    result = cyclicRotation(A, size, 3);
+    print_int_array(result.A, size);
+    free(result.A);
     return 0;
 }
