fb83a7ed624119d7ff1b86958e471cf049e0c7e5
gt <gurkanturkarslan@hotmail.com>
2022-02-02 15:32:08 +0300

    Update insertion_sort.c

--- src/c/exercist/sorting/insertion_sort.c
+++ src/c/exercist/sorting/insertion_sort.c
@@ -12,6 +12,23 @@
  *
  * [7,3,5,8,2,9,4,15,6] dizisinin Insertion Sort'a göre ilk 4 adımını yazınız.
  *
+ * 1.            [22, 27, 16, 2, 18, 6]
+ *      1. step: [22, 27, 16, 2, 18, 6]
+ *      2. step: [16, 22, 27, 2, 18, 6]
+ *      3. step: [2, 16, 22, 27, 18, 6]
+ *      4. step: [2, 16, 18, 22, 27, 6]
+ *      5. step: [2, 6, 16, 18, 22, 27]
+ *
+ *
+ * 2.   Best: O(n), Average: O(n^2), Worst: O(n^2)
+ *
+ * 3.   After sorting the array, 18 will be in the average case.
+ *
+ * 4.            [7, 3, 5, 8, 2, 9, 4, 15, 6]
+ *      1. step: [3, 7, 5, 8, 2, 9, 4, 15, 6]
+ *      2. step: [3, 5, 7, 8, 2, 9, 4, 15, 6]
+ *      3. step: [3, 5, 7, 8, 2, 9, 4, 15, 6]
+ *      4. step: [2, 3, 5, 7, 8, 9, 4, 15, 6]
  */
 
 #include <stdio.h>
@@ -35,7 +52,7 @@ void insertion_sort(int arr[], size_t size) {
             arr[j+1] = arr[j];
         }
         arr[j + 1] = tmp;
-        // echo each step to the console
+        // echo each step
         printf("%li. step: ", i);
         print_int_array(arr, size);
     }
@@ -47,6 +64,5 @@ int main() {
     size_t size_arr = sizeof arr / sizeof arr[0];
     print_int_array(arr, size_arr);
     insertion_sort(arr, size_arr);
-//    print_int_array(arr, size_arr);
     return 0;
 }
