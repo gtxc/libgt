45bc9557cb62aa1104fd7011111268c40793d322
gt <gurkanturkarslan@hotmail.com>
2022-02-03 03:10:25 +0300

    Update merge_sort.cpp

--- src/c/exercist/sorting/merge_sort.cpp
+++ src/c/exercist/sorting/merge_sort.cpp
@@ -9,18 +9,36 @@
  * Yukarıdaki dizinin sort türüne göre aşamalarını yazınız.
  * Big-O gösterimini yazınız.
  *
+ *      [3, 2, 1, 5, 8, 0, 7]
+ *      [3, 2, 1, 5]
+ *      [3, 2]
+ *      [3]
+ *      [2]
+ *      [2, 3]
+ *      [1, 5]
+ *      [1]
+ *      [5]
+ *      [1, 5]
+ *      [1, 2, 3, 5]
+ *      [8, 0, 7]
+ *      [8, 0]
+ *      [8]
+ *      [0]
+ *      [0, 8]
+ *      [7]
+ *      [0, 7, 8]
+ *      [0, 1, 2, 3, 5, 7, 8]
  *
- *
- *
+ *  Time Complexity: O(n*log(n))
  */
 
 #include <iostream>
 
 using namespace std;
 
-void print_int_array(int *A, int size) {
+void print_int_array(int *A, int low,  int size) {
     cout << "[";
-    for (int i = 0; i < size; ++i) {
+    for (int i = low; i < size; ++i) {
         if (i == size-1) cout << A[i];
         else cout << A[i] << ", ";
     }
@@ -32,6 +50,7 @@ void merge(int A[], int low, int mid, int high) {
     int i = low;
     int j = mid+1;
     int k = low;
+
     while (i <= mid && j <= high) {
         if (A[i] <= A[j]) {
             tmp[k++] = A[i++];
@@ -48,24 +67,28 @@ void merge(int A[], int low, int mid, int high) {
     for (k = low; k <= high; ++k) {
         A[k] = tmp[k];
     }
+    // echo each step
+    print_int_array(A, low, high+1);
+
 }
 
-void divide(int A[], int low, int high) {
+void merge_sort(int A[], int low, int high) {
+    // echo each step
+    print_int_array(A, low, high+1);
     if (low >= high) {
         return;
     }
     int mid = (low + high) / 2;
-    divide(A, low, mid);
-    divide(A, mid+1, high);
+    merge_sort(A, low, mid);
+    merge_sort(A, mid+1, high);
     merge(A, low, mid, high);
 }
-
 
 int main() {
     int A[] = {3,2,1,5,8,0,7};
     int size = sizeof A / sizeof A[0];
-    print_int_array(A, size);
-    divide(A, 0, size);
-    print_int_array(A, size);
+//    print_int_array(A, size);
+    merge_sort(A, 0, size-1);
+//    print_int_array(A, size);
     return 0;
 }
